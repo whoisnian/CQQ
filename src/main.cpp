c679297be95ca71be9f08d74fbbef45ed23dcf73
whoisnian <zhuchangbao1998@gmail.com>
2019-10-26 21:45:21 +0800

    :bug: fix display emoji error on mac

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    #if defined(Q_OS_MACOS)
+        QApplication::setFont(QFont("PingFang"));
+        QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    #endif
     int ret = 0;
     do
     {
